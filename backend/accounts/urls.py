c45345d0d500560b3134fff65fcc75421829e78c
Development-osgpro <iy8ld2@gmail.com>
2022-06-17 16:47:18 +0300

    pip compile base.txt requirements, upgrade Django to 4.0.5, and added email confirmation upon signing up

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import (
-    UserViewSet,
-    RegisterView,
-    MyTokenObtainPairView
-)
+from . import views
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
 router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', MyTokenObtainPairView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
     path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('confirmation/<str:pk>/<str:uid>/', views.confirmation, name='email_confirmation'),
     path("", include(router.urls)),
 ]
